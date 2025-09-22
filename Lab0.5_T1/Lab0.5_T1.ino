0ce6f40f81420c03abc46ecc9416b6ae4db3dca4
pnanakorn <pnanakorn@connect.ust.hk>
2025-09-22 17:38:01 +0800

    Update lab 0.5

--- Lab0.5_T1/Lab0.5_T1.ino
+++ Lab0.5_T1/Lab0.5_T1.ino
@@ -1,5 +1,5 @@
 // the number of the LED pin
-const int ledPin = 5;  // 5 corresponds to GPIO5
+const int vrPin = 36;  // 5 corresponds to GPIO5
 
 // setting PWM properties
 const int freq = 5000;
@@ -13,26 +13,19 @@ void setup(){
   //freq select frequency of pwm.
   //resolution select resolution for LEDC channel. Range is 1-14 bits (1-20 bits for ESP32).
   //The LEDC channel will be selected automatically
-
-  ledcAttach(ledPin, freq, resolution);
+  Serial.begin(115200);
+  pinMode(vrPin, INPUT);
+  ledcAttach(vrPin, freq, resolution);
 
 }
  
 void loop(){
 
   //bool ledcWrite(uint8_t pin, uint32_t duty);
+  int vr_value = analogRead(vrPin);
+  int dutyCycle = map(vr_value, 0, 4095, 0, 255);
+  Serial.println(dutyCycle);
 
-  // increase the LED brightness
-  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
-    // changing the LED brightness with PWM
-    ledcWrite(ledPin, dutyCycle);
-    delay(15);
-  }
-
-  // decrease the LED brightness
-  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
-    // changing the LED brightness with PWM
-    ledcWrite(ledPin, dutyCycle);   
-    delay(15);
-  }
+  ledcWrite(vrPin, dutyCycle);
+  delay(100);
 }
